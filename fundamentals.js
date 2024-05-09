3363e3868c37a2b89c42efe8bb40acd8ff1c6c0e
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-05-09 18:09:11 +0600

    first commit

---
+++ fundamentals.js
@@ -0,0 +1,8 @@
+/*
+1. How to declare a variable using let and const
+2. Condition: >, <, ===, !==, <=, >=, &&, ||
+3. Array: declare, index, length, push, pop
+4. Loop: for loop, while loop
+5. Function
+6. Object: dot notation, bracket notation
+*/
